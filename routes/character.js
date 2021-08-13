3ce318c594bba57110324a28193f41c0a5842520
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2021-08-13 14:09:29 +0400

    feat: add new route : PUT /character/update/:id

--- routes/character.js
+++ routes/character.js
@@ -97,4 +97,105 @@ router.get("/character/:id", async (req, res) => {
   }
 });
 
+// -- UPDATE --
+router.put("/character/update/:id", async (req, res) => {
+  try {
+    const characterToUpdate = await Character.findById(req.params.id);
+
+    const {
+      name,
+      height,
+      mass,
+      hair_color,
+      skin_color,
+      eye_color,
+      gender,
+      films,
+      species,
+      vehicles,
+      starships,
+    } = req.fields;
+
+    if (
+      // si on trouve un personne à modifier, et si au moins un paramètre est à modifier
+      characterToUpdate &&
+      (name ||
+        height ||
+        mass ||
+        hair_color ||
+        skin_color ||
+        eye_color ||
+        gender ||
+        films ||
+        species ||
+        vehicles ||
+        starships)
+    ) {
+      // (() => {
+      //   Object.keys(req.fields).forEach((e, i) => {
+      //     characterToUpdate[e] = Object.values(req.fields)[i];
+      //   });
+      // })();
+
+      if (name) {
+        characterToUpdate.name = name;
+      }
+
+      if (height) {
+        characterToUpdate.height = height;
+      }
+
+      if (mass) {
+        characterToUpdate.mass = mass;
+      }
+
+      if (hair_color) {
+        characterToUpdate.hair_color = hair_color;
+      }
+
+      if (skin_color) {
+        characterToUpdate.skin_color = skin_color;
+      }
+
+      if (eye_color) {
+        characterToUpdate.eye_color = eye_color;
+      }
+
+      if (gender) {
+        characterToUpdate.gender = gender;
+      }
+
+      if (films) {
+        characterToUpdate.films = films;
+      }
+
+      if (species) {
+        characterToUpdate.species = species;
+      }
+
+      if (vehicles) {
+        characterToUpdate.vehicles = vehicles;
+      }
+
+      if (starships) {
+        characterToUpdate.starships = starships;
+      }
+
+      characterToUpdate.edited = Date.now();
+
+      await characterToUpdate.save();
+
+      res.status(200).json(characterToUpdate);
+    } else if (!characterToUpdate) {
+      // si aucun personnage trouvé avec l'id renseigné
+      res.status(400).json({ message: "Character not found" });
+    } else {
+      // si aucun paramètre n'est à modifier
+      res.status(400).json({ message: "No parameters to update" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = router;
