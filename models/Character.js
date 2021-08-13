cc131f338823a030d0b1b0545dbb8889be14d1df
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2021-08-13 10:27:06 +0400

    feat: configure Character model/schema with Mongoose API

---
+++ models/Character.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGOOSE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Character = mongoose.model("Character", {
+  name: { type: String, required: true },
+  height: { type: Number, required: true },
+  mass: { type: Number, required: true },
+  hair_color: { type: String, required: true },
+  skin_color: { type: String, required: true },
+  eye_color: { type: String, required: true },
+  birth_year: String,
+  gender: { type: String, required: true },
+  homeworld: String,
+  films: Array,
+  species: Array,
+  vehicles: Array,
+  starships: Array,
+  created: { type: Date, default: Date.now() },
+  edited: { type: Date, default: Date.now() },
+});
+
+module.exports = Character;
